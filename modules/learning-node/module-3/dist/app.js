8b942d19379e2da6ec3ea0e6dc6939f188297782
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-01 00:40:18 +0600

    learning express js todo app

--- modules/learning-node/module-3/dist/app.js
+++ modules/learning-node/module-3/dist/app.js
@@ -4,11 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
+app.get("/", (req, res) => {
+    res.send("Welcome to todos app..");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    res.json(data);
 });
+exports.default = app;
--- modules/learning-node/module-3/src/app.ts
+++ modules/learning-node/module-3/src/app.ts
@@ -1,10 +1,20 @@
-import express, { Application, Request, Response } from "express"
-const app: Application = express()
+import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+const app: Application = express();
+
+const filePath = path.join(__dirname, "../db/todo.json");
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to todos app..");
+});
+
+app.get("/todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+  res.json(data);
+});
 
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
 
 
 export default app;
