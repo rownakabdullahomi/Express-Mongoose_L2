4b01020d63146f1a7580986b04aedee93b823528
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-01 01:06:13 +0600

    working with query and params

--- modules/learning-node/module-3/dist/app.js
+++ modules/learning-node/module-3/dist/app.js
@@ -7,12 +7,20 @@ const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const filePath = path_1.default.join(__dirname, "../db/todo.json");
 app.get("/", (req, res) => {
     res.send("Welcome to todos app..");
 });
-app.get("/todos", (req, res) => {
+app.get("/todos/:title/:body", (req, res) => {
+    console.log("From Query", req.query);
+    console.log("From Params", req.params);
     const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
     res.json(data);
+});
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(`${title} --> ${body}`);
+    res.send("Welcome to todos app..");
 });
 exports.default = app;
--- modules/learning-node/module-3/src/app.ts
+++ modules/learning-node/module-3/src/app.ts
@@ -3,18 +3,25 @@ import fs from "fs";
 import path from "path";
 const app: Application = express();
 
+app.use(express.json());
+
 const filePath = path.join(__dirname, "../db/todo.json");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to todos app..");
 });
 
-app.get("/todos", (req: Request, res: Response) => {
+app.get("/todos/:title/:body", (req: Request, res: Response) => {
+  console.log("From Query", req.query);
+  console.log("From Params", req.params);
   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
   res.json(data);
 });
 
-
-
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const { title, body } = req.body;
+  console.log(`${title} --> ${body}`);
+  res.send("Welcome to todos app..");
+});
 
 export default app;
