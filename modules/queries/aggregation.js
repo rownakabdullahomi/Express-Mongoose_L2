6a1879ed4bb7ad6fb5be2c362318523040164b0e
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-07 23:44:50 +0600

    practicing aggregation

--- modules/queries/aggregation.js
+++ modules/queries/aggregation.js
@@ -58,3 +58,32 @@ db.test.aggregate([
     }
 
 ])
+
+// -------------------- more $group and $project -------------------------------
+db.test.aggregate([
+
+    // stage 1
+    {
+        $group: {
+            _id: null,
+            totalSalary: { $sum: "$salary" },
+            maxSalary: { $max: "$salary" },
+            minSalary: { $min: "$salary" },
+            avgSalary: { $avg: "$salary" },
+
+        }
+    },
+
+    // stage 2
+    {
+        $project: {
+            totalSalary: 1,
+            maxSalary: 1,
+            minSalary: 1,
+            averageSalary: "$avgSalary",
+            rangeBetweenMaxAndMin: { $subtract: ["$maxSalary", "$minSalary"] }
+        }
+    }
+
+
+])
