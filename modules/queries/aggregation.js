e1e7e0d77a5e903c8eacfd4fd653ac76f6d72dd4
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-09 02:20:54 +0600

    practicing aggregation

--- modules/queries/aggregation.js
+++ modules/queries/aggregation.js
@@ -143,3 +143,44 @@ db.test.aggregate([
     }
 
 ])
+
+// ------------------ $facet ----------------------------
+db.test.aggregate([
+    {
+        $facet: {
+            // pipeline 1
+            "friendsCount": [
+                // stage 1
+                {
+                    $unwind: "$friends"
+                },
+                // stage 2
+                {
+                    $group: { _id: "$friends", count: { $sum: 1 } }
+                }
+            ],
+            // pipeline 2
+            "educationCount": [
+                // stage 1
+                {
+                    $unwind: "$education",
+                },
+                // stage 2
+                {
+                    $group: { _id: "$education", count: { $sum: 1 } }
+                }
+            ],
+            // pipeline 3
+            "skillsCount": [
+                // stage 1
+                {
+                    $unwind: "$skills"
+                },
+                // stage 2
+                {
+                    $group: { _id: "$skills", count: { $sum: 1 } }
+                }
+            ]
+        }
+    }
+])
