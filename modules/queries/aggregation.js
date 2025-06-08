7898b96996061c3228923e39383c749fc5da1473
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-08 20:44:08 +0600

    practicing aggregation

--- modules/queries/aggregation.js
+++ modules/queries/aggregation.js
@@ -84,6 +84,30 @@ db.test.aggregate([
             rangeBetweenMaxAndMin: { $subtract: ["$maxSalary", "$minSalary"] }
         }
     }
+])
 
+// ----------------- unwind -----------------------------------------------
 
+db.test.aggregate([
+    // stage 1
+    {
+        $unwind: "$friends"
+    },
+
+    // stage 2
+    {
+        $group: { _id: "$friends", count: { $sum: 1 } }
+    }
+
+])
+
+db.test.aggregate([
+    // stage 1
+    {
+        $unwind: "$interests"
+    },
+    // stage 2
+    {
+        $group: { _id: "$age", interestsPerAge: { $push: "$interests" } }
+    }
 ])
