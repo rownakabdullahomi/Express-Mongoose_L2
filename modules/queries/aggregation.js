f088cb0a67086697957366443e7d09b826563ae3
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-09 19:01:49 +0600

    practicing aggregation indexing

--- modules/queries/aggregation.js
+++ modules/queries/aggregation.js
@@ -197,3 +197,12 @@ db.orders.aggregate([
         }
     }
 ])
+
+//  ----------------------- indexing ----------------------------
+
+db.getCollection("massive-data").createIndex({email: 1})
+
+// db.getCollection("massive-data").createIndex({about: "text"})
+db.getCollection("massive-data").find({ $text: { $search: "dolor" } }).project({ about: 1 })
+
+  
