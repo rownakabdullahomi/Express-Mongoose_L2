4fb32686e658b1980d1afdad809a71bf73760b22
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-10 22:23:58 +0600

    practicing aggregation indexing

--- modules/queries/aggregation.js
+++ modules/queries/aggregation.js
@@ -205,5 +205,12 @@ db.getCollection("massive-data").createIndex({email: 1})
 // db.getCollection("massive-data").createIndex({about: "text"})
 db.getCollection("massive-data").find({ $text: { $search: "dolor" } }).project({ about: 1 })
 
+//  ----------------------- indexing -----------------------------------
+
+db.getCollection("massive-data").createIndex({email: 1})
+
+// db.getCollection("massive-data").createIndex({about: "text"})
+db.getCollection("massive-data").find({ $text: { $search: "dolor" } }).project({ about: 1 })
+
   
 // ------------------------------------------------------------------------
