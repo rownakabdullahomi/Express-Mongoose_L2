12a7beae2304785e362d005244c0bae0e793f9d8
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-11 21:01:09 +0600

    working with rest api project structure

--- modules/queries/aggregation.js
+++ modules/queries/aggregation.js
@@ -204,5 +204,3 @@ db.getCollection("massive-data").createIndex({email: 1})
 
 // db.getCollection("massive-data").createIndex({about: "text"})
 db.getCollection("massive-data").find({ $text: { $search: "dolor" } }).project({ about: 1 })
-  
-// ------------------------------------------------------------------------
