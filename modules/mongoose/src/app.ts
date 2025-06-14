712bf6482bfc5cda5e17c87e5ecbb65ff4875cbd
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-15 01:36:42 +0600

    CRUD done with mongoose

--- modules/mongoose/src/app.ts
+++ modules/mongoose/src/app.ts
@@ -4,7 +4,8 @@ import { model, Schema } from "mongoose";
 const app: Application = express();
 app.use(express.json());
 
-const noteSchema = new Schema({
+const noteSchema = new Schema(
+    {
   title: { type: String, required: true, trim: true },
   content: { type: String, default: "" },
   category: {
@@ -20,7 +21,12 @@ const noteSchema = new Schema({
     label: { type: String, required: true },
     color: { type: String, default: "gray" },
   },
-});
+}, {
+    versionKey: false,
+    timestamps: true
+}
+
+);
 
 const Note = model("Note", noteSchema);
 
@@ -46,7 +52,7 @@ app.get("/notes", async (req: Request, res: Response) => {
 });
 
 app.get("/notes/:noteId", async (req: Request, res: Response) => {
-    const nodeId = req.params.noteId
+  const nodeId = req.params.noteId;
   const note = await Note.findById(nodeId);
 
   res.status(201).json({
@@ -56,6 +62,28 @@ app.get("/notes/:noteId", async (req: Request, res: Response) => {
   });
 });
 
+app.patch("/notes/:noteId", async (req: Request, res: Response) => {
+  const nodeId = req.params.noteId;
+  const updatedBody = req.body;
+  const note = await Note.findByIdAndUpdate(nodeId, updatedBody, {new: true});
+
+  res.status(201).json({
+    success: true,
+    message: "Note updated successfully.",
+    data: note,
+  });
+});
+
+app.delete("/notes/:noteId", async (req: Request, res: Response) => {
+  const nodeId = req.params.noteId;
+  const note = await Note.findByIdAndDelete(nodeId);
+
+  res.status(201).json({
+    success: true,
+    message: "Note deleted successfully.",
+    data: note,
+  });
+});
 
 
 
