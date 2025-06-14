9648057da0d1fb34d4861e6163ac75f0626f1747
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-15 02:03:53 +0600

    make the structure cleaner

--- modules/mongoose/src/app.ts
+++ modules/mongoose/src/app.ts
@@ -1,97 +1,11 @@
 import express, { Application, Request, Response } from "express";
-import { model, Schema } from "mongoose";
+import { Note } from "./app/models/notes.model";
+import { notesRoutes } from "./app/controllers/notes.controller";
 
 const app: Application = express();
 app.use(express.json());
 
-const noteSchema = new Schema(
-    {
-  title: { type: String, required: true, trim: true },
-  content: { type: String, default: "" },
-  category: {
-    type: String,
-    enum: ["personal", "work", "family", "other"],
-    default: "personal",
-  },
-  pinned: {
-    type: Boolean,
-    default: false,
-  },
-  tags: {
-    label: { type: String, required: true },
-    color: { type: String, default: "gray" },
-  },
-}, {
-    versionKey: false,
-    timestamps: true
-}
-
-);
-
-const Note = model("Note", noteSchema);
-
-app.post("/notes/create-note", async (req: Request, res: Response) => {
-  const body = req.body;
-  const note = await Note.create(body);
-
-  res.status(201).json({
-    success: true,
-    message: "Note created successfully.",
-    data: note,
-  });
-});
-
-app.get("/notes", async (req: Request, res: Response) => {
-  const notes = await Note.find();
-
-  res.status(201).json({
-    success: true,
-    message: "Note created successfully.",
-    data: notes,
-  });
-});
-
-app.get("/notes/:noteId", async (req: Request, res: Response) => {
-  const nodeId = req.params.noteId;
-  const note = await Note.findById(nodeId);
-
-  res.status(201).json({
-    success: true,
-    message: "Note created successfully.",
-    data: note,
-  });
-});
-
-app.patch("/notes/:noteId", async (req: Request, res: Response) => {
-  const nodeId = req.params.noteId;
-  const updatedBody = req.body;
-  const note = await Note.findByIdAndUpdate(nodeId, updatedBody, {new: true});
-
-  res.status(201).json({
-    success: true,
-    message: "Note updated successfully.",
-    data: note,
-  });
-});
-
-app.delete("/notes/:noteId", async (req: Request, res: Response) => {
-  const nodeId = req.params.noteId;
-  const note = await Note.findByIdAndDelete(nodeId);
-
-  res.status(201).json({
-    success: true,
-    message: "Note deleted successfully.",
-    data: note,
-  });
-});
-
-
-
-
-
-
-
-
+app.use("/notes", notesRoutes)
 
 
 
---
+++ modules/mongoose/src/app/controllers/notes.controller.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import { Note } from '../models/notes.model';
+
+export const notesRoutes = express.Router();
+
+notesRoutes.post("/create-note", async (req: Request, res: Response) => {
+  const body = req.body;
+  const note = await Note.create(body);
+
+  res.status(201).json({
+    success: true,
+    message: "Note created successfully.",
+    data: note,
+  });
+});
+
+notesRoutes.get("/", async (req: Request, res: Response) => {
+  const notes = await Note.find();
+
+  res.status(201).json({
+    success: true,
+    message: "Note created successfully.",
+    data: notes,
+  });
+});
+
+notesRoutes.get("/:noteId", async (req: Request, res: Response) => {
+  const nodeId = req.params.noteId;
+  const note = await Note.findById(nodeId);
+
+  res.status(201).json({
+    success: true,
+    message: "Note created successfully.",
+    data: note,
+  });
+});
+
+notesRoutes.patch("/:noteId", async (req: Request, res: Response) => {
+  const nodeId = req.params.noteId;
+  const updatedBody = req.body;
+  const note = await Note.findByIdAndUpdate(nodeId, updatedBody, { new: true });
+
+  res.status(201).json({
+    success: true,
+    message: "Note updated successfully.",
+    data: note,
+  });
+});
+
+notesRoutes.delete("/:noteId", async (req: Request, res: Response) => {
+  const nodeId = req.params.noteId;
+  const note = await Note.findByIdAndDelete(nodeId);
+
+  res.status(201).json({
+    success: true,
+    message: "Note deleted successfully.",
+    data: note,
+  });
+});
