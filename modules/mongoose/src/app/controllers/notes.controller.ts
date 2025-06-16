d8687825a58076a8efebafca3f31961e7c24f5e6
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-17 02:05:55 +0600

    working with populate method

--- modules/mongoose/src/app/controllers/notes.controller.ts
+++ modules/mongoose/src/app/controllers/notes.controller.ts
@@ -15,7 +15,7 @@ notesRoutes.post("/create-note", async (req: Request, res: Response) => {
 });
 
 notesRoutes.get("/", async (req: Request, res: Response) => {
-  const notes = await Note.find();
+  const notes = await Note.find().populate("user");
 
   res.status(201).json({
     success: true,
