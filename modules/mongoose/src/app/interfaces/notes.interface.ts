4ae19fa914316813a24cdaeb7d5f3d30635f0352
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-16 18:58:26 +0600

    adding interface for notes

--- modules/mongoose/src/app/interfaces/notes.interface.ts
+++ modules/mongoose/src/app/interfaces/notes.interface.ts
@@ -1,3 +1,11 @@
 export interface INotes{
-    
+    title: string,
+    content: string,
+    category: "personal" | "work" | "family" | "study" | "other"
+    pinned: boolean,
+    tags: {
+        label: string,
+        color: string
+    }
+
 }
