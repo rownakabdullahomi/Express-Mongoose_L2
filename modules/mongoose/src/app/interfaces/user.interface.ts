af1a9b5576b78908b4425e90e04594ac089cdca5
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-06-16 21:14:37 +0600

    working with validations

--- modules/mongoose/src/app/interfaces/user.interface.ts
+++ modules/mongoose/src/app/interfaces/user.interface.ts
@@ -1,7 +1,8 @@
 export interface IUser {
     firstName: string,
     lastName: string,
+    age: number,
     email: string,
     password: string,
-    role: "user" | "admin"
+    role: "USER"| "ADMIN"| "SUPERADMIN"
 }
--- modules/mongoose/src/app/models/user.model.ts
+++ modules/mongoose/src/app/models/user.model.ts
@@ -2,30 +2,52 @@ import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>({
-    firstName:{
-        type: String,
-        required: true,
-        trim: true
+  firstName: {
+    type: String,
+    required: [true, "Please give the first name, it is mandatory."],
+    trim: true,
+    minlength: 3,
+    maxlength: 12,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: [18, "Age must be at least 18, got {VALUE}"],
+    max: 60,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: function (value) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: function (props){
+        return `The email ${props.value} is not a valid email`
+      }
     },
-    lastName:{
-        type: String,
-        required: true,
-        trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    uppercase: true,
+    enum: {
+      values: ["USER", "ADMIN", "SUPERADMIN"],
+      message: "{VALUE} is not a valid role. ",
     },
-    email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user"
-    }
-})
+    default: "USER",
+  },
+});
 
-export const User = model("User", userSchema)
+export const User = model("User", userSchema);
